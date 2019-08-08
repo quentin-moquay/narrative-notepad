db4ceb881b1548105edff83b2c001dd1cdb0cb17
quentin-moquay <26006295+quentin-moquay@users.noreply.github.com>
2019-08-08 19:18:38 +0200

    Update pdf-lib because old one was not node10 compatible

--- src/renderer/back/ExportPdf.js
+++ src/renderer/back/ExportPdf.js
@@ -6,7 +6,7 @@ export default class ExportPdf {
   constructor () {
     if (!ExportPdf.instance) {
       ExportPdf.instance = this
-      this.pdf = require('electron-phantom-html2pdf')
+      this.pdf = require('html-pdf')
       this.options = Options.instance.config
     }
     return ExportPdf.instance
@@ -42,7 +42,14 @@ export default class ExportPdf {
 
   generatePdf (content) {
     let contentsTableData = '<div style="page-break-after:always;"><table><tbody>'
-    let data = '<div style="page-break-after:always;"><h1>My Book</h1></div>'
+    let data = '<style>' +
+      'body {\n' +
+      '    font-family: \'Ubuntu\', sans-serif;\n' +
+      '    font-size: 12px;' +
+      '    text-align: justify;' +
+      '}' +
+      '</style>'
+    data += '<div style="page-break-after:always;"><h1>My Book</h1></div>'
     _.sortBy(content, 'order').forEach((chapter, idx) => {
       contentsTableData += '<tr>'
       contentsTableData += `<td>${idx + 1}.${chapter.story.description}</td>`
@@ -57,27 +64,11 @@ export default class ExportPdf {
     data += contentsTableData
 
     const options = {
-      html: data,
-      css: 'body {\n' +
-        '    font-family: \'Ubuntu\', sans-serif;\n' +
-        '    font-size: 12px;' +
-        '    text-align: justify;' +
-        '}',
-      deleteOnAction: true,
-      paperSize: {
-        format: 'A4',
-        orientation: 'portrait',
-        margin: '2.5cm'
-      }
+      border: '2.5cm'
     }
-    this.pdf.convert(options, (err, result) => {
-      if (err) {
-        console.log(err)
-      } else {
-        result.toFile(`${this.options.workingDir}output.pdf`, () => {
-          console.log('File wrote')
-        })
-      }
+    this.pdf.create(data, options).toFile(`${this.options.workingDir}output.pdf`, function (err, res) {
+      if (err) return console.log(err)
+      console.log(res)
     })
   }
 }
